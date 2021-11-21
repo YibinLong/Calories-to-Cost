5516934269f750a2651b138b1f87d9b46076deec
Yibin Long <yibinlong@outlook.com>
2021-11-21 17:39:05 -0500

    test to print ABC News

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
+
+
 url = 'https://www.allsides.com/media-bias/media-bias-ratings'
 r = requests.get(url)
-print(r.content[:100])
+#print(r.content[:100])
+
+soup = BeautifulSoup(r.content, 'html.parser')
+rows = soup.select('tbody tr')
+
+row = rows[0]
+name = row.select_one('.source-title').text.strip()
+
+print(name)
