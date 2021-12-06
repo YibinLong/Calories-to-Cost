2927e90eb6f477386cbd692c48bf2a852b4aa4c2
Yibin Long <yibinlong@outlook.com>
2021-12-06 08:04:44 -0500

    more progress, now added bias to scraping

--- test.py
+++ test.py
@@ -9,7 +9,17 @@ r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 rows = soup.select('tbody tr')
 
+#select the first row
 row = rows[0]
 name = row.select_one('.source-title').text.strip()
-
 print(name)
+
+#select the news source
+allsides_page = row.select_one('.source-title a')['href']
+allsides_page = 'https://www.allsides.com' + allsides_page
+print(allsides_page)
+
+#grab the bias from the site
+bias = row.select_one('.views-field-field-bias-image a')['href']
+bias = bias.split('/')[-1]
+print(bias)
